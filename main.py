f6041b59d8ffa268670af5c6f5da2910857fdf19
Zack-Sutter <zxsutter@gmail.com>
2026-06-01 16:05:41 -0400

    added text color display and rules skeleton.

--- main.py
+++ main.py
@@ -4,16 +4,36 @@ def compute_next(prev_row: list, window_size: int, rules: dict = {"key":"value"}
     next_row = []
     for i in range(0, len(prev_row)+1-window_size):
         slice = prev_row[i:i+window_size]
-        next_row += slice
+        next_row += rules[tuple(slice)]
     return next_row
 
+rules = {
+    (0,0,0): [0],
+    (0,0,1): [0],
+    (0,1,0): [0],
+    (0,1,1): [1],
+    (1,0,0): [1,0],
+    (1,0,1): [1,0],
+    (1,1,0): [1,1],
+    (1,1,1): [0,1]
+}
 
-seed = [1,2,3,4,5,2]
+seed = [0,0,1,0,0,1,1,0,1,1]
+
+red = "🟥"
+green = "🟩"
 
 my_rows = []
 my_rows.append(seed)
-for i in range(2):
-    my_rows.append(compute_next(my_rows[i], 3))
+for i in range(30):
+    my_rows.append(compute_next(my_rows[i], window_size = 3, rules = rules))
 
 for row in my_rows:
-    print(row)
+    row_string = ''
+    for dig in row:
+        if dig == 0:
+            row_string += red
+        else:
+            row_string += green
+
+    print(row_string)
